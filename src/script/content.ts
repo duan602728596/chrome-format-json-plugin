5eb27b52acbadc33ed2ab75e8ca7780af31ad78d
duan602728596 <duanhaochen@126.com>
2019-01-31 18:06:30 +0800

    格式化链接

--- src/script/content.ts
+++ src/script/content.ts
@@ -5,6 +5,22 @@ chrome.runtime.onMessage.addListener(function(request: { type: string }, sender:
   }
 });
 
+/* 格式化链接 */
+function formatHref(): void{
+  const hljsString: NodeListOf<Element> = document.querySelectorAll('.hljs-string');
+
+  for(const element of hljsString){
+    const str: string = element.innerHTML;
+
+    if(!/^"https?:\/\/.+"$/.test(str)) continue;
+
+    const href = str.substr(1, str.length - 2);
+    const aNode: string = `"<a class="link" href="${ href }" target="_blank" rel="noopener noreferrer">${ href }</a>"`;
+
+    element.innerHTML = aNode;
+  }
+}
+
 /* 格式化JSON */
 async function formatJSON(): Promise<void>{
   try{
@@ -29,6 +45,9 @@ async function formatJSON(): Promise<void>{
       preRawString[0].style.display = 'none';
 
       window['hljs'].highlightBlock(element);
+
+      // 查找所有的链接，并格式化链接
+      formatHref();
     }
   }catch(err){
     alert('JSON格式错误！');
--- src/script/popup.ts
+++ src/script/popup.ts
@@ -3,28 +3,21 @@ import Tab = chrome.tabs.Tab;
 const formatJSONBtn: Element = document.getElementById('formatJSON');
 
 /* 判断css和js是否注入 */
-let css: boolean = false;
-let js: boolean = false;
+let isInject: boolean = false;
 
 /* 注入css */
-function injectCSS(): Promise<void>{
+function injectCSS(file: string): Promise<void>{
   return new Promise((resolve: Function, reject: Function): void=>{
-    chrome.tabs.insertCSS(null, {
-      file: 'dependencies/github-gist.css'
-    }, function(): void{
-      css = true;
+    chrome.tabs.insertCSS(null, { file }, function(): void{
       resolve();
     });
   });
 }
 
 /* 注入js */
-function injectJS(): Promise<void>{
+function injectJS(file: string): Promise<void>{
   return new Promise((resolve: Function, reject: Function): void=>{
-    chrome.tabs.executeScript(null, {
-      file: 'dependencies/highlight.pack.min.js'
-    }, function(): void{
-      js = true;
+    chrome.tabs.executeScript(null, { file }, function(): void{
       resolve();
     });
   });
@@ -33,11 +26,20 @@ function injectJS(): Promise<void>{
 /* 格式化JSON */
 async function handleFormatJSONClick(event: Event): Promise<void>{
   // 加载cdn
-  if(js === false) await injectJS();
-  if(css === false) await injectCSS();
+  if(isInject === false){
+    await Promise.all([
+      injectCSS('dependencies/github-gist.css'),
+      injectCSS('style/page.css'),
+      injectJS('dependencies/highlight.pack.min.js'),
+    ]);
+
+    isInject = true;
+  }
 
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs: Tab[]): void{
-    chrome.tabs.sendMessage(tabs[0].id, { type: 'FORMAT_JSON' });
+    chrome.tabs.sendMessage(tabs[0].id, {
+      type: 'FORMAT_JSON'
+    });
   });
 }
 
